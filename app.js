9c98ad0dda7cf1c13b78bc2e8578f1a7dd170015
Dmitry Boyko <dmitry@thebodva.com>
2015-11-23 20:02:51 +0200

    show list of personas

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var app = angular.module('appKingLear', []);
 app.controller('DramatisPersonaeController', ['$scope', '$http', function($scope, $http){
     $http.get('personas.json').success(function(data) {
         $scope.personas = data;
+        console.log(data);
     });
 }]);
